ff2fbfc4b9ec5a0b2fd080a66098dd354cfce7f8
Henry <hcen20@stuy.edu>
2020-12-20 16:53:09 -0500

    Initial commit

---
+++ Test.ino
@@ -0,0 +1,16 @@
+int potPin = A0;
+int buttonPin = 2;
+int ledPin = 3;
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(potPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int potState = analogRead(potPin) / 4;
+  analogWrite(ledPin, potState);
+  int buttonState = digitalRead(buttonPin);
+  digitalWrite(ledPin, not buttonState);
+}
