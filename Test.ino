2551e6348f1f35cc6fc8d6324fba800562843809
Henry <hcen20@stuy.edu>
2020-12-21 23:08:22 -0500

    OK Boomer

--- Test.ino
+++ Test.ino
@@ -1,21 +1,30 @@
-int potPin = A0;
 int buttonPin = 2;
-int ledPin = 3;
+int lastButtonState = 1;
+long unsigned int lastPress;
+int debounceTime = 20;
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(potPin, INPUT);
-  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop() {
-  int potState = analogRead(potPin); 
-  int buttonState = digitalRead(buttonPin);
-  Serial.print("Button: ");
-  Serial.print(buttonState);
-  Serial.println("");
-  Serial.print("Pot: ");
-  Serial.print(potState);
-  Serial.println("");
-}
+void loop() {  
+  int buttonState = digitalRead(buttonPin); 
+  Serial.print(lastPress);
+
+  if((millis() - lastPress) > debounceTime)
+    lastPress = millis();                            
+                          
+  if(buttonState == 0 && lastButtonState == 1) {  
+    Serial.println("OK Boomer");
+    lastButtonState = 0;   
+    }
+
+  if(buttonState == 1 && lastButtonState == 0) {
+      lastButtonState = 1;    
+    }
+  }
+
+
+
+ 
