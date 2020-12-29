7f0cb0aad23bf6fb7cc7467fb1318c569d8e6e46
Henry <hcen20@stuy.edu>
2020-12-29 01:03:21 -0500

    Analog Servo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Homemade-Amazon-Lights
 
+Path to making homemade Amazon lights:
+12/28 -  Learned how to make a toggle push buttton, and started learning how to program a Servo motor
--- Servo.ino
+++ Servo.ino
@@ -1,15 +1,19 @@
 #include <Servo.h>
 
 int servoPin = 9;
-int servoPos = 0;
+int servoPos;
+int potPin = A0;
 Servo myServo;
 
 void setup() {
     Serial.begin(9600);
     myServo.attach(servoPin);
+    pinMode(potPin, INPUT);
 }
 
 void loop() {
+    int potState = analogRead(potPin)/4;
+    servoPos = potState;
     myServo.write(servoPos);
 
 }
